abe322c3969db9aa87864368a2efef9fa7c8c514
Ariya <ariyasacca20@gmail.com>
2026-05-31 20:55:49 +0700

    update

--- src/components/section/RSVP.tsx
+++ src/components/section/RSVP.tsx
@@ -42,6 +42,7 @@ export default function RSVP() {
     }
   }, [API]);
 
+  //test
   // Fetch sekali saat mount
   // eslint-disable-next-line react-hooks/set-state-in-effect
   useEffect(() => { loadWishes(); }, [loadWishes]);
