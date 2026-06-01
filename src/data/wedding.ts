4f7dc1f27376590fa8f7ab73132362da2b133b15
Ariya <ariyasacca20@gmail.com>
2026-06-01 15:57:00 +0700

    update

--- src/data/wedding.ts
+++ src/data/wedding.ts
@@ -55,7 +55,7 @@ export const weddingData = {
           "Perum Bojong Menteng Indah, Jl. Kemuning Raya Dalam, RT 03/13, Bojong Menteng, Rawalumbu, Kota Bekasi, Jawa Barat 17117",
 
         maps:
-          "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3965.7032622176534!2d106.98607227316857!3d-6.302664861679031!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69920e14e83bcf%3A0xd9e8ecb5c3168997!2sSTI%20Vih%C4%81ra%20Vipassan%C4%81%20Kusalacitta!5e0!3m2!1sid!2sid!4v1780206148629!5m2!1sid!2sid",
+          "https://maps.app.goo.gl/pkLBZ4u37Ai72HMZ6",
       },
     },
 
@@ -74,7 +74,7 @@ export const weddingData = {
           "Jl. Damar 5 Terusan No.12, RT.006/RW.006, Pekayon Jaya, Kec. Bekasi Selatan, Kota Bekasi, Jawa Barat 17148",
 
         maps:
-          "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3965.955882207236!2d106.98236897316806!3d-6.269532661372033!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e698dc0c2ea3dbd%3A0x705bfac29d0981a7!2sOmah%20Kebon%20Bekasi!5e0!3m2!1sen!2sid!4v1780124869346!5m2!1sen!2sid",
+          "https://maps.app.goo.gl/zrVrsEvFLcGmZ1DK7",
       },
     },
   },
